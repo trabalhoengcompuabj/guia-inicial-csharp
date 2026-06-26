9feadac7852f19457f7c8c531cec82775eb53bba
Felipe Silva Martins <felipesilvmartins@gmail.com>
2026-06-26 00:33:17 -0300

    adição do exemplo do loop for

---
+++ exemplos/fundamentos/loops.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Loops
+{
+    public static void Main(string[] args)
+    {
+        // Exemplo do loop for
+        // Executa o código por uma quantidade de vezes definida
+        for (int numero = 1; numero <= 10; numero = numero + 1)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
