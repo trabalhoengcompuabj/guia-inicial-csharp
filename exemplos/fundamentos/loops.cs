37155a06f020529096435af69e4172bc2948b0b8
Felipe Silva Martins <felipesilvmartins@gmail.com>
2026-06-26 19:46:47 -0300

    adição de espaçamento entre os exemplos para facilitar a visualização

--- exemplos/fundamentos/loops.cs
+++ exemplos/fundamentos/loops.cs
@@ -11,6 +11,8 @@ public class Loops
             Console.WriteLine(numero);
         }
 
+
+
         // Exemplo do loop while
         // Executa o código enquanto a condição for atendida
         int regressivo = 10;
@@ -20,6 +22,8 @@ public class Loops
             Console.WriteLine(regressivo);
             regressivo = regressivo - 1;
         }
+
+
 
         // Exemplo do loop do-while
         // Executa o código pelo menos uma vez antes de testar a condição
@@ -31,6 +35,8 @@ public class Loops
             contagem = contagem + 1;
             
         } while (contagem <= 10);
+
+
 
         // Exemplo do loop foreach
         // Executa o código para cada elemento de um array ou lista
