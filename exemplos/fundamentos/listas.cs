2bbe0d3df70352ac96d4a3aefe87e64d84e1afeb
Felipe Silva Martins <felipesilvmartins@gmail.com>
2026-06-27 17:19:36 -0300

    adição do exemplo de listas

---
+++ exemplos/fundamentos/listas.cs
@@ -0,0 +1,84 @@
+class Listas
+{
+    static void Main()
+    {
+        // Criação de uma lista
+        List<string> jogos = new List<string>();
+
+
+
+        // Adição de elementos a uma lista
+        jogos.Add("Jogo quadrado");
+
+
+
+        // Adiciona mais de um elemento a uma lista de uma única vez
+        // Pode adicionar um array ou uma outra lista dentro da lista
+        string[] adicional = {"Zelda", "Pokemon", "Terraria"};
+        jogos.AddRange(adicional);
+
+
+
+        // Modificar valores através do índice
+        // OBS.: O primeiro elemento começa em índice zero
+        jogos[0] = "Minecraft";
+
+
+
+        // Mostrar a quantidade de elementos na lista
+        Console.WriteLine(jogos.Count);
+
+
+
+        // Verificar se a lista contém um elemento específico 
+        if (jogos.Contains("Minecraft"))
+        {
+            Console.WriteLine("Correspondência encontrada na lista");
+        }
+
+
+
+        // Reorganizar a lista
+        // Ordem alfabética para textos
+        // Ordem crescente para números
+        jogos.Sort();
+
+
+
+        // Reorganiza a lista na ordem inversa
+        jogos.Reverse();
+
+
+
+        // Mostrar posição do Índice de um elemento
+        Console.WriteLine(jogos.IndexOf("Terraria"));
+
+
+
+        // Remover elemento da lista
+        jogos.Remove("Zelda");
+
+
+
+        // Remover elemento da lista através do índice
+        jogos.RemoveAt(1);
+
+
+
+        // Transformar a lista em um array
+        string[] jogosArray = jogos.ToArray();
+
+
+        
+        // Visualizar todos os elementos de uma lista
+        foreach(string jogo in jogos)
+        {
+            Console.WriteLine(jogo);
+        }
+
+
+
+        // Limpar todos os elementos de uma lista
+        jogos.Clear();
+    }
+}
