ca41eaccdef5a4283293a9960c59ed14b6e62db2
RYAN TORRES COSTA TRINDADE <ryan.torres@ufrpe.br>
2026-06-28 19:53:15 -0300

    Update helloword.cs

--- exemplos/fundamentos/helloword.cs
+++ exemplos/fundamentos/helloword.cs
@@ -1,12 +1,9 @@
 using System;
 
-namespace HelloWorld
+public class HelloWorld
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine ("hello, world");
     }
 }
